e9c63aff591ca1afb94a4091dcd84a136b4ef583
Joey Lee <jleepianist@gmail.com>
2016-11-26 22:05:45 -0800

    Added `some`

--- src/underbar.js
+++ src/underbar.js
@@ -207,6 +207,13 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    for(var i = 0; i < collection.length; i++){
+      if(iterator(collection[i])){
+        return true;
+      }
+    };
+    return false;
   };
 
 
